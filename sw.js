f6c4bfe768a90fc857fcfd87d03b5c41a63be302
pateljay834 <pateljay834@gmail.com>
2025-08-28 20:47:43 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,41 @@
-const CACHE_NAME = 'jayara-cache-v1';
+const CACHE_NAME = "jayara-cache-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './apps.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  "./",
+  "./index.html",
+  "./style.css",
+  "./apps.js",
+  "./manifest.json"
 ];
 
-self.addEventListener('install', event => {
+// Install SW and cache files
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
-    ))
-  );
-});
-
-self.addEventListener('fetch', event => {
+// Serve from cache if offline
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate & clean old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
