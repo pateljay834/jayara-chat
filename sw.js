187f53a08259baabdd24bd1961fdc4f06ac0dc16
pateljay834 <pateljay834@gmail.com>
2025-08-29 01:57:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,15 +4,32 @@ const urlsToCache = [
   "./index.html",
   "./style.css",
   "./apps.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./manifest.json"
 ];
 
-// ----------------- Install -----------------
+// Install Service Worker and cache files
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+// Fetch from cache if offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// Activate & clean old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
