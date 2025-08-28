83244fcfcded21d45870d1d0c642fb0bb114ca2e
pateljay834 <pateljay834@gmail.com>
2025-08-29 01:01:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,44 +4,15 @@ const urlsToCache = [
   "./index.html",
   "./style.css",
   "./apps.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-// Cache install
+// ----------------- Install -----------------
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-// Serve from cache
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
-
-// Clean old caches
-self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
-});
-
-// Firebase Push Notifications
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-  apiKey: "AIzaSyB0G0JLoNejrshjLaKxFR264cY11rmhVJU",
-  authDomain: "jayara-web.firebaseapp.com",
-  projectId: "jayara-web",
-  messagingSenderId: "342182893596",
-  appId: "1:342182893596:web:664646e95a40e60d0da7d9"
-});
-
-const messaging = firebase.messaging();
-messaging.onBackgroundMessage(payload => {
-  const notificationTitle = payload.notification?.title || "Jayara Chat";
-  const notificationOptions = {
-    body: payload.notification?.body || "New message received",
-    icon: 'icon-192.png'
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
